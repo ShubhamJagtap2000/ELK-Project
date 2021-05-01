531bb1c22d2a9240a2065ce9660ac04145f9be76
ShubhamJagtap2000 <63872951+ShubhamJagtap2000@users.noreply.github.com>
2021-05-02 02:19:41 +0530

    .py file

--- Create Index/create.py
+++ Create Index/create.py
@@ -1,4 +1,13 @@
 #Refer to README.md file for a detailed code and execution steps
+import elasticsearch
+import eland as ed
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(
+    ['host_server_name'],
+    http_auth=('YOUR_USERNAME', 'YOUR_PASSWORD'),
+    scheme="https",
+    port=443)
 
 #Following syntax is used to create an index into your Elasticsearch
 es.indices.create(index = "My_First_Index", ignore = 400)
